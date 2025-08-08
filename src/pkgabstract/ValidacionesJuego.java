c86aa1274058c45da8123abb6569bbb47ba183ae
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-08-08 10:37:34 -0600

    Clase que hereda de validador, validaciones del juego

--- src/pkgabstract/ValidacionesJuego.java
+++ src/pkgabstract/ValidacionesJuego.java
@@ -17,12 +17,53 @@ public class ValidacionesJuego extends Validador {
     @Override
     
     public boolean verificar(int fila, int col, int x){
-        return true;
+       if( x == 0){
+           return true;
+       }
+       return verificarFila(fila, col, x) && verificarColumna(fila, col, x) && verificarCelda(fila, col, x);
     }
     
    @Override
-    public boolean partidaTerminada(){
-        return false;
+    public boolean partidaTerminada(){ //Verificar si ya se solucionó el cuadro
+       for (int i = 0; i < 9; i++) {
+           for (int j = 0; j < 9; j++) {
+               if(tabla[i][j] == 0 || verificar(i, j, tabla[i][j])){
+                   return false;
+               }
+           }
+       }
+       return true;
     }
     
+    private boolean verificarFila(int fila, int col, int x){
+        for (int i = 0; i < 9; i++) {
+            if(i != col && tabla[fila][i] == x){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean verificarColumna(int fila, int col, int x){
+        for (int i = 0; i < 9; i++) {
+           if(i != fila && tabla[i][col] == x){
+               return false;
+           }
+        }
+        return true;
+    }
+    
+    private boolean verificarCelda(int fila, int col, int x){
+        int subCeldaFila = (fila/3)*3;
+        int subCeldaColumna = (col/3)*3;
+        
+        for (int i = subCeldaFila; subCeldaFila+3 > i; i++) {
+            for (int j = subCeldaColumna; subCeldaColumna > i; j++) {
+                if(i != fila && j != col && tabla[i][j] == x){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
