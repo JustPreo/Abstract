f8f32af304f76ad1abc43f271e4078fd0c6ed0c6
JustPreo <impre0hn@gmail.com>
2025-08-08 11:46:14 -0600

    a

--- src/pkgabstract/Menu.java
+++ src/pkgabstract/Menu.java
@@ -5,7 +5,10 @@
 package pkgabstract;
 
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -16,6 +19,7 @@ import javax.swing.JLabel;
 public class Menu extends JFrame {
 
     private JLabel fondo1;
+    public JButton b1, b2;
 
     public Menu() {
         ImageIcon originalIcon = new ImageIcon(getClass().getResource("/pkgabstract/SREK.PNG"));
@@ -29,6 +33,30 @@ public class Menu extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         plantilla();
+
+        b1 = new JButton("JUGAR");
+        b1.setBounds(200, 150, 100, 50); //X , Y , WIDTH , HEIGHT
+        fondo1.add(b1);
+
+        b2 = new JButton("SALIR");
+        b2.setBounds(200, 250, 100, 50); //X , Y , WIDTH , HEIGHT
+        fondo1.add(b2);
+
+        //Action Perfomed
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new SudokuGUI().setVisible(true);
+                dispose();
+
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+
+            }
+        });
 
     }
 
