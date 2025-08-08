cd3f3cd519b4c6bb4b9b231fa9f465d139aa3383
JustPreo <impre0hn@gmail.com>
2025-08-08 10:17:20 -0600

    a

--- src/pkgabstract/celda.java
+++ src/pkgabstract/celda.java
@@ -9,26 +9,40 @@ package pkgabstract;
  * @author user
  */
 public class celda extends ElementosABS {
-    private int r,c;
+
+    private int r, c;
     private boolean unico;
-    
-    public celda(int r , int c , int valor , boolean unico)
-    {
-    super(valor , !unico);
-    this. r = r;
-    this.c = c;
-    this.unico = unico;
-    
-    }
-    
-    public boolean valido() {
-        return valor >=
-        
+
+    public celda(int r, int c, int valor, boolean unico) {
+        super(valor, !unico);
+        this.r = r;
+        this.c = c;
+        this.unico = unico;
+
     }
 
-    
-    public void resetear() {
-    
+    public boolean valido() {
+        return valor >= 0 && valor <= 9;
+
     }
-    
+
+    public void resetear() {
+        if (editable) {
+            valor = 0;
+        }
+
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public boolean unico() {
+        return unico;
+    }
+
 }
